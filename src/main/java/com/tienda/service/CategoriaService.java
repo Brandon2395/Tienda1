a35d605a49f11ba23dd88bbb4a0a0b2af63f705f
Brandon2395 <acostabrandon2395@gmail.com>
2023-10-29 20:57:03 -0600

    Update: Code

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -7,16 +7,18 @@ package com.tienda.service;
 import com.tienda.domain.Categoria;
 import java.util.List;
 
-/**
- *
- * @author Brandon
- */
 public interface CategoriaService {
     
-    //Se declara un método para obtener un ArrayList de objetos Categoria
-    //Los objetos vienen de la tabla categoria, todos los registros.
-    
+    // Se obtiene un listado de categorias en un List
     public List<Categoria> getCategorias(boolean activos);
     
-    //Abajo se colocara los metodos para realizar el CRUD de las categorias, semana 6
+   // Se obtiene un Categoria, a partir del id de un categoria
+    public Categoria getCategoria(Categoria categoria);
+    
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Categoria categoria);
+    
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);
 }
